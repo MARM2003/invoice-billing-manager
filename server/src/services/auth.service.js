4cdc8bd5eea1b6695dc13da101bf78c673fb7bbc
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-09 00:11:44 +0530

    feat: implemented service for authentication

---
+++ server/src/services/auth.service.js
@@ -0,0 +1,78 @@
+import bcrypt from "bcryptjs";
+import prisma from "../prismaClient/prismaClient.js";
+import { generateToken } from "../utils/generateToken.js";
+
+//register logic
+export const registerUser=async(userData)=>{
+    //destructing the data
+    const {name,email,password}=userData;
+
+    //checking for existing user
+    const existingUser= await prisma.user.findUnique(
+       {
+        where:{
+            email,
+        },
+       }
+)
+    //if user exist then throw error
+    if(existingUser) throw new Error("User already exists")
+
+    //hashing the password
+    const hashedPassword= await bcrypt.hash(password,10)
+
+    //creating the user registeration data
+    const user = await prisma.user.create({
+        data:{
+            name,
+            email,
+            password:hashedPassword,
+        },
+    })
+
+    //token generation
+     const token = generateToken(user.id);
+
+     //returning the token. 
+     return {
+        user:{
+            id:user.id,
+            name:user.name,
+            email:user.email
+        },
+        token
+     }
+}
+
+//login logic
+export const loginUser=async(email,password)=>{
+    //finding the user
+    const user = await prisma.user.findUnique({
+        where:{
+            email,
+        },
+    })
+
+    //if no user find
+    if(!user) throw new Error("Invalid credentials")
+
+    //comparing the password.
+    const isMatch = await bcrypt.compare(password,user.password)
+
+    //if password not match
+    if(!isMatch) throw new Error("Invalid credentials")
+
+     //token generation
+     const token = generateToken(user.id);
+
+     //returning the token. 
+     return {
+        user:{
+            id:user.id,
+            name:user.name,
+            email:user.email
+        },
+        token
+     }
+    
+}
