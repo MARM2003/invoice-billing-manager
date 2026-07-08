bc6ae9da142c48f7e954fcc9bfc098c7ae44dabb
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-09 00:06:47 +0530

    feat: Created a universal prisma client

--- server/src/prismaClient/prismaClient.js
+++ server/src/prismaClient/prismaClient.js
@@ -1,5 +1,20 @@
-import { PrismaClient } from "@prisma/client";
+// import pkg from '@prisma/client';
+// const { PrismaClient } = pkg;
+// // import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+
+// export default prisma;
+
+import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export default prisma;
