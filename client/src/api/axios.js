4780221358f42aa958f3836004ab161dc71a9d94
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-13 23:39:16 +0530

    using token storage utils.,updated the request ans response interceptor

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,22 +1,19 @@
 import axios from "axios";
-
+import { refreshTokenAPI } from "./auth.api";
+import { getAccessToken, setAccessToken, clearAuth } from "../utils/tokenStorage";
+import { toast } from "react-toastify";
 const api = axios.create({
   baseURL: "http://localhost:5000/api/v1",
-  withCredentials:true,
+  withCredentials: true,
 });
 
 
 api.interceptors.request.use(
   (config) => {
-    const auth = localStorage.getItem("user");
-    
-    if (auth) {
-      const { accessToken } = JSON.parse(auth);
+    const accessToken = getAccessToken();
 
-      if (accessToken) {
-        console.log("Access Token:", accessToken);
-        config.headers.Authorization = `Bearer ${accessToken}`;
-      }
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
 
     return config;
@@ -24,4 +21,51 @@ api.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+api.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const originalRequest = error.config;
+
+    // Safety check
+    if (originalRequest.url === "/auth/refresh") {
+      return Promise.reject(error);
+    }
+
+    if (
+      error.response?.status === 401 &&
+      originalRequest.headers?.Authorization &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const response = await refreshTokenAPI();
+
+        const newAccessToken = response.data.accessToken;
+
+        setAccessToken(newAccessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return api(originalRequest);
+
+      } catch (refreshError) {
+        console.error("Refresh token expired or invalid.");
+
+        clearAuth();
+
+        toast.error("Session expired. Please login again.");
+
+        setTimeout(() => {
+          window.location.href = "/login";
+        }, 1500);
+
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 export default api;
