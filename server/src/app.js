292bb307db42bf563cbd7acc58f9afc6214becef
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-13 11:41:44 +0530

    added the cors,cookie-parser, user-rote

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import cors from "cors"
 //imported the auth routes.
 import authRoutes from "./routes/v1/auth.routes.js"
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/v1/user.routes.js"
 
 const app = express();
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
-
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.json({
     success: true,
@@ -15,6 +21,9 @@ app.get("/", (req, res) => {
 
 //register and login route
 app.use("/api/v1/auth", authRoutes);
+
+//user routes
+app.use("/api/v1/users",userRoutes)
 
 
 export default app
