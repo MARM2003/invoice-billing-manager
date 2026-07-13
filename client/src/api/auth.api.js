04536de85682e417e087aadebe23d6bfd821c1ee
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-13 23:36:58 +0530

    feat: created a logout and refresh API

--- client/src/api/auth.api.js
+++ client/src/api/auth.api.js
@@ -1,10 +1,18 @@
 import api from "./axios";
-
+import refreshApi from "./refreshApi";
 export const registerAPI = (data) => {
   return api.post("/auth/register", data);
 };
 
 export const loginAPI = (data) => {
   return api.post("/auth/login", data);
+};
+
+export const logoutAPI = () => {
+  return api.post("/auth/logout")
+}
+
+export const refreshTokenAPI = () => {
+  return refreshApi.post("/auth/refresh");
 };
 
