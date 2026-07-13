e4102ee2866995869c7d8a251be154cbefbf4350
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-13 23:34:14 +0530

    enable the browser router for auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,11 +6,11 @@ import App from './App.jsx'
 import React from "react"
 import { AuthProvider } from "./context/AuthContext.jsx";
 createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
         <App />
-      </BrowserRouter>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
