eaf9a72a6aca1a7e2b0e7a171b2aac0aa0f443b6
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-13 11:53:44 +0530

    updated the import and names based on the jwt utils file and change of key names

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,82 +1,85 @@
 import bcrypt from "bcryptjs";
 import prisma from "../prismaClient/prismaClient.js";
-import { generateToken } from "../utils/generateToken.js";
+import { generateAccessToken, generateRefreshToken } from "../utils/jwt.js";
 
 //register logic
-export const registerUser=async(userData)=>{
+export const registerUser = async (userData) => {
     //destructing the data
-    const {name,email,password,companyName}=userData;
+    const { name, email, password, companyName } = userData;
 
     //checking for existing user
-    const existingUser= await prisma.user.findUnique(
-       {
-        where:{
-            email,
-        },
-       }
-)
+    const existingUser = await prisma.user.findUnique(
+        {
+            where: {
+                email,
+            },
+        }
+    )
     //if user exist then throw error
-    if(existingUser) throw new Error("User already exists")
+    if (existingUser) throw new Error("User already exists")
 
     //hashing the password
-    const hashedPassword= await bcrypt.hash(password,10)
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     //creating the user registeration data
     const user = await prisma.user.create({
-        data:{
+        data: {
             name,
             email,
-            password:hashedPassword,
+            password: hashedPassword,
             companyName
         },
     })
 
     //token generation
-     const token = generateToken(user.id);
-
-     //returning the token. 
-     return {
-        user:{
-            id:user.id,
-            name:user.name,
-            email:user.email,
-            company_name:user.companyName,
+    const token = generateAccessToken(user.id);
+    //returning the token. 
+    return {
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            company_name: user.companyName,
         },
         token
-     }
+    }
 }
 
 //login logic
-export const loginUser=async(email,password)=>{
+export const loginUser = async (email, password) => {
     //finding the user
     const user = await prisma.user.findUnique({
-        where:{
+        where: {
             email,
         },
     })
 
     //if no user find
-    if(!user) throw new Error("Invalid credentials")
+    if (!user) throw new Error("Invalid credentials")
 
     //comparing the password.
-    const isMatch = await bcrypt.compare(password,user.password)
+    const isMatch = await bcrypt.compare(password, user.password)
 
     //if password not match
-    if(!isMatch) throw new Error("Invalid credentials")
+    if (!isMatch) throw new Error("Invalid credentials")
 
-     //token generation
-     const token = generateToken(user.id);
+    //token generation
+    const token = generateAccessToken(user.id);
+    //refresh token
+    const refreshToken = generateRefreshToken(user.id)
 
-     //returning the token. 
-     return {
-        user:{
-            id:user.id,
-            name:user.name,
-            email:user.email,
-            company_name:user.companyName,
-            profilestatus:user.profileCompleted
+
+    //returning the token. 
+    return {
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            company_name: user.companyName,
+            profilestatus: user.profileCompleted
         },
-        token
-     }
-    
+        token,
+        refreshToken
+    }
+
 }
