af712ceb9a919dfce6e48c291be3a98390508737
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-13 11:23:35 +0530

    updated the main files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
+import AppRoutes from "./routes/AppRoute"
+import { ToastContainer } from "react-toastify";
 function App() {
   
   return (
     <>
-      
+      <AppRoutes/>
+       <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        theme="colored"
+      />
     </>
   )
 }
