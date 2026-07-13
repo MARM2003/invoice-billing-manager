1cb55b9d185b450a4f63bd63a3888a20ca48f286
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-13 11:48:23 +0530

    implemeted the validate middleware in auth routes

--- server/src/routes/v1/auth.routes.js
+++ server/src/routes/v1/auth.routes.js
@@ -1,15 +1,18 @@
 import express from "express"
 //importing the controllers
 import { register, login } from "../../controllers/auth.controller.js"
-
+//importing the validation middleware
+import { validate } from "../../middleware/validate.middleware.js"
+//importing the schema validation
+import { registerSchema,loginSchema } from "../../validations/auth.validation.js"
 //created a router app
 const router = express.Router()
 
 //POST /api/v1/auth/register
-router.post("/register", register)
+router.post("/register",validate(registerSchema), register)
 
 //POST /api/v1/auth/login
-router.post("/login", login)
+router.post("/login", validate(loginSchema),login)
 
 
 export default router
