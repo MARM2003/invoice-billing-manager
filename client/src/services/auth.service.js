4a5b1c5cffc6ac58b1d17432aa253dc75930612c
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-13 23:41:50 +0530

    implemented the logout service

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,14 +1,22 @@
-import { registerAPI,loginAPI } from "../api/auth.api";
+//importing the auth API's
+import { registerAPI, loginAPI, logoutAPI } from "../api/auth.api";
 
+
+//register user service
 export const registerUser = async (userData) => {
   const response = await registerAPI(userData);
 
   return response.data;
 };
 
+//login user service
 export const loginUser = async (userData) => {
   const response = await loginAPI(userData);
 
   return response.data;
 };
 
+//logout user service
+export const logoutUser = async () => {
+  const response = await logoutAPI();
+}
