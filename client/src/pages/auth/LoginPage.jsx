c0e3efbe7faceae4649ef4e9b6db7de612a3280d
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-13 11:32:59 +0530

    auth pages :login and register implemented

---
+++ client/src/pages/auth/LoginPage.jsx
@@ -0,0 +1,8 @@
+import Login from "../../components/forms/LoginForm";
+
+ const LoginPage = ()=>{
+    return (
+        <Login/>
+    )
+}
+export default LoginPage
---
+++ client/src/pages/auth/RegisterPage.jsx
@@ -0,0 +1,8 @@
+import Register from "../../components/forms/RegisterForm";
+
+ const RegisterPage = ()=>{
+    return (
+        <Register/>
+    )
+}
+export default RegisterPage
