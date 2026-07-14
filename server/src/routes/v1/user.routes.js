d3d871003d5d44d58fdd008ddd776fe8f57c1ded
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-14 22:20:25 +0530

    put the validation on the router

--- server/src/routes/v1/user.routes.js
+++ server/src/routes/v1/user.routes.js
@@ -1,13 +1,16 @@
 import express from "express";
 import authMiddleware from "../../middleware/auth.middleware.js";
 import { updateProfile } from "../../controllers/user.controller.js";
+import { completeProfileSchema } from "../../validations/auth.validation.js"
+import { validate } from "../../middleware/validate.middleware.js";
 import upload from "../../middleware/upload.middleware.js";
 const router = express.Router();
 
 router.put(
   "/profile",
   authMiddleware,
-  upload.single("logo"),   
+  upload.single("logo"),
+  validate(completeProfileSchema),
   updateProfile
 );
 
