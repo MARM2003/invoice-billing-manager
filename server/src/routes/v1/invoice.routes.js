301870055b3b3143cdd398df32b631b1fc233d52
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-31 17:03:31 +0530

    Implemented routes for creating invoice, fetching all invoices, fetch invoice using Id, update invoice, deleting a invoice.

--- server/src/routes/v1/invoice.routes.js
+++ server/src/routes/v1/invoice.routes.js
@@ -1,8 +1,8 @@
 import express from "express"
 import { validate } from "../../middleware/validate.middleware.js"
 import authMiddleware from "../../middleware/auth.middleware.js"
-import { createInvoiceSchema } from "../../validations/invoice.validation.js"
-import { createInvoice } from "../../controllers/invoice.controller.js"
+import { createInvoiceSchema, updateInvoiceSchema } from "../../validations/invoice.validation.js"
+import { createInvoice, getInvoices, getInvoiceById, updateInvoice, deleteInvoice } from "../../controllers/invoice.controller.js"
 const router = express.Router();
 
 
@@ -13,4 +13,11 @@ router.post(
     createInvoice
 );
 
+router.get("/", authMiddleware, getInvoices)
+
+router.get("/:id", authMiddleware, getInvoiceById)
+
+router.put("/:id", authMiddleware, validate(updateInvoiceSchema), updateInvoice)
+
+router.delete("/:id", authMiddleware, deleteInvoice)
 export default router;
