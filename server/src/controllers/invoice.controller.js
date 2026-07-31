f924e39f0ac6e528de2fecdc94850ff8f3b12edf
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-07-31 17:03:05 +0530

    Implemented controllers  for creating invoice, fetching all invoices, fetch invoice using Id, update invoice, deleting a invoice with proper error handling

--- server/src/controllers/invoice.controller.js
+++ server/src/controllers/invoice.controller.js
@@ -1,4 +1,5 @@
-import { createInvoiceService } from "../services/invoice.service.js";
+import { success } from "zod";
+import { createInvoiceService, getInvoicesService, getInvoiceByIdService, updateInvoiceService, deleteInvoiceService } from "../services/invoice.service.js";
 
 export const createInvoice = async (req, res, next) => {
   try {
@@ -15,3 +16,76 @@ export const createInvoice = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getInvoices = async (req, res, next) => {
+  try {
+    const userId = req.user.userId;
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const search = req.query.search || ""
+    const customerId = req.query.customerId;
+
+    const result = await getInvoicesService(userId, page, limit, search, customerId)
+
+    return res.status(200).json({
+      success: true,
+      message: "Invoices fetched successfully",
+      data: result.invoices,
+      pagination: result.pagination
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getInvoiceById = async (req, res, next) => {
+  try {
+    const userId = req.user.userId
+    const invoiceId = req.params.id
+
+    const invoice = await getInvoiceByIdService(invoiceId, userId);
+
+    return res.status(200).json({
+      success: true,
+      message: "Invoice fetched successfully",
+      data: invoice
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const updateInvoice = async (req, res, next) => {
+  try {
+    const userId = req.user.userId
+    const invoiceId = req.params.id
+    const invoiceData = req.body
+    const updatedInvoice = await updateInvoiceService(invoiceId, userId, invoiceData)
+
+    return res.status(200).json({
+      success: true,
+      message: "Invoice updated successfully",
+      data: updatedInvoice
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+
+export const deleteInvoice = async (req, res, next) => {
+  try {
+    const userId = req.user.userId
+    const invoiceId = req.params.id
+
+    const deletedInvoice = await deleteInvoiceService(invoiceId, userId)
+
+    return res.status(200).json({
+      success: true,
+      message: "Invoice deleted successfully",
+      data: deletedInvoice
+    })
+  } catch (error) {
+    next(error)
+  }
+}
