f920b25420deb3e7e7e152856cab35587bf86c6e
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-08-19 18:19:58 +0530

    feat: implement email service for sending emails with attachments

---
+++ server/src/services/email.service.js
@@ -0,0 +1,31 @@
+import "dotenv/config"
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: process.env.SMTP_PORT === "465",
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+    },
+});
+
+export const sendEmail = async ({
+    to,
+    subject,
+    html,
+    attachments = [],
+}) => {
+    const mailOptions = {
+        from: process.env.SMTP_FROM,
+        to,
+        subject,
+        html,
+        attachments,
+    };
+
+    return await transporter.sendMail(mailOptions);
+};
+
+
