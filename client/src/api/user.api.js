101083fa6e20201363635755ae087cd6c42df88b
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-09-02 16:12:16 +0530

    feat: add user profile retrieval and update functionality in user service

--- client/src/api/user.api.js
+++ client/src/api/user.api.js
@@ -6,4 +6,11 @@ const updateProfileApi = async (formData) => {
     return api.put("/users/profile", formData);
 };
 
+export const getProfileAPI = async () => {
+    return api.get("/users/profile")
+}
+
+export const updateUserProfileAPI = async (data) => {
+    return api.put("/users/user-profile", data)
+}
 export default updateProfileApi
--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,11 +1,24 @@
 // user.service.js
 
-import  updateProfileApi from "../api/user.api.js";
+import updateProfileApi, { getProfileAPI, updateUserProfileAPI } from "../api/user.api.js";
 
 const updateProfile = async (formData) => {
   const response = await updateProfileApi(formData);
 
   return response.data;
 };
+
+export const getProfile = async () => {
+  const response = await getProfileAPI();
+
+  return response.data
+}
+
+export const updateUserProfile = async (data) => {
+  const response=await updateUserProfileAPI(data)
+
+  
+  return response.data
+}
 
 export default updateProfile
