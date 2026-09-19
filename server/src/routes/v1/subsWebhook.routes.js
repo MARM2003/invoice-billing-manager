e6cc7ae165498157ccafaf1c77d24a3fec506acd
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2026-09-19 23:46:23 +0530

    feat: add subsWebhook route to handle Stripe webhook events

---
+++ server/src/routes/v1/subsWebhook.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import {
+    handleStripeWebhook,
+} from "../../controllers/subsWebhook.controller.js";
+
+const router = express.Router();
+
+router.post("/", handleStripeWebhook);
+
+export default router;
